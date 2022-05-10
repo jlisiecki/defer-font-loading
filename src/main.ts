259a88dfbfaa7d4a08f6c229075029f5f97955f1
Jakub Lisiecki <jakub@lisiecki.xyz>
2022-05-10 10:24:22 +0200

    seems prepared

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-export {};
+import file from '@fontsource/cherry-swash/files/cherry-swash-latin-400-normal.woff2';
+
+(async function (d: Document) {
+    const fontName = 'Cherry Swash';
+    const font = new FontFace(fontName, `url(${file})`);
+    font.load().then(() => {
+        (d.fonts as any).add(font);
+        d.querySelectorAll<HTMLElement>('.container').forEach((element) => {
+            element.style.fontFamily = `'${fontName}', cursive`;
+        });
+    });
+})(document);
