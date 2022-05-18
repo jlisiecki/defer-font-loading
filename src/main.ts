977c49db11053cd6d489ccaaccf1c21467566931
Jakub Lisiecki <jakub@lisiecki.xyz>
2022-05-18 10:00:06 +0200

    go on

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,36 @@
-import file from '@fontsource/cherry-swash/files/cherry-swash-latin-400-normal.woff2';
+import cherrySwash from '@fontsource/cherry-swash/files/cherry-swash-latin-400-normal.woff2';
+import aclonica from '@fontsource/aclonica/files/aclonica-latin-400-normal.woff2';
+import boogaloo from '@fontsource/boogaloo/files/boogaloo-latin-400-normal.woff2';
+import cambay from '@fontsource/cambay/files/cambay-latin-400-normal.woff2';
+import krub from '@fontsource/krub/files/krub-latin-400-normal.woff2';
 
-(async function (d: Document) {
-    const fontName = 'Cherry Swash';
-    const font = new FontFace(fontName, `url(${file})`);
+interface FontInfo {
+    file: string;
+    name: string;
+}
+
+const fontsInfo: FontInfo[] = [
+    { file: cherrySwash, name: 'cherry-swash' },
+    { file: aclonica, name: 'aclonica' },
+    { file: boogaloo, name: 'boogaloo' },
+    { file: cambay, name: 'cambay' },
+    { file: krub, name: 'krub' }
+];
+
+function deferFont(fontInfo: FontInfo, d: Document) {
+    const font = new FontFace(fontInfo.name, `url(${fontInfo.file})`);
     font.load().then(() => {
         (d.fonts as any).add(font);
-        d.querySelectorAll<HTMLElement>('.container').forEach((element) => {
-            element.style.fontFamily = `'${fontName}', cursive`;
-        });
+        d.querySelectorAll<HTMLElement>('.' + fontInfo.name).forEach(
+            (element) => {
+                element.style.fontFamily = fontInfo.name;
+            }
+        );
     });
-})(document);
+}
+
+function deferFonts(fontsInfo: FontInfo[], d: Document) {
+    for (let fontInfo of fontsInfo) deferFont(fontInfo, d);
+}
+
+deferFonts(fontsInfo, document);
